8617aa17bac8acf6835bfec0cb7455025f80025f
Casper Christensen <caschr21@student.aau.dk>
2025-09-01 18:34:49 +0200

    Test

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,16 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  process.exit(1);
+});
+
 // Handle __dirname in ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
